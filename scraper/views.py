322c03f54601a7cbcaac2925140d8d692cada97e
norman <caminonebila96@gmail.com>
2025-11-26 15:11:10 -0300

    agregue views a scraper

--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def buscar_educativo(request):
+    query = request.GET.get('q', '')
+    resultados = []
+    
+    if query:
+        resultados = [{
+            'titulo': f'Resultado para "{query}"',
+            'contenido': f'Información educativa sobre {query}',
+            'fuente': 'Sistema Educativo'
+        }]
+    
+    return render(request, 'scraper/buscar.html', {
+        'resultados': resultados,
+        'query': query
+    })
